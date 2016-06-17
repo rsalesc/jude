ed24ef93fd0ccb77f88c10c1da42986909239e29
Roberto <iseme.beto@gmail.com>
2016-06-17 11:05:00 -0300

    add JudgeConfig and new dependencies

--- judge/environment.js
+++ judge/environment.js
@@ -5,14 +5,31 @@
 var FileCacher = require('./fcacher')
 var await = require('asyncawait/await')
 var async = require('asyncawait/async')
-    
+var path = require('path')
+
+const MAX_SANDBOXES = 10
+
+var JudgeConfig = {
+    EPS : 1e-7,
+    MAX_SANDBOXES : 10,
+    TEMP_DIR : "/tmp",
+    ISOLATE_PATH : path.resolve("isolate/isolate")
+}
+
 class JudgeEnvironment{
     constructor(){
         this.sandboxes = []
+        this.nextSandboxId = 0
         this.cacher = new FileCacher()
+    }
+
+    getNextBoxId(){
+        let res = this.nextSandboxId++
+        return res % JudgeConfig.MAX_SANDBOXES
     }
 }
 
 module.exports = {
-    JudgeEnvironment
+    JudgeEnvironment,
+    JudgeConfig
 }
