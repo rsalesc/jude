df7abd95e255d05c34f18f872acb2ad40357362a
Roberto <iseme.beto@gmail.com>
2016-11-04 21:58:25 -0300

    set docker hostnames with env variables

--- db.js
+++ db.js
@@ -3,13 +3,17 @@ var mongodbQueue = require('mongodb-queue');
 const weed = require('jude-seaweedfs');
 
 // mongodb setup
-var uri = 'mongodb://db/jude-dev';
+
+let mongo_host = process.env.MONGO_HOST || 'localhost';
+let weed_host = process.env.WEED_HOST || 'localhost';
+
+var uri = 'mongodb://'+mongo_host+'/jude-dev';
 if(!global.db) {
     global.db = mongoose.createConnection(uri);
     global.db.mods = {};
     global.judeQueue = mongodbQueue(db, "jude-queue");
     global.weedClient     = new weed({
-        server:     "fs",
+        server:     weed_host,
         port:       9333
     });
 }
--- judge/weed.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-weed server -master.port=9333 -dir=./fs -volume.port=8080
