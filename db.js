7647283f3b512da4e5eb07216083519fb96c36a9
Roberto <iseme.beto@gmail.com>
2017-07-15 08:40:37 -0300

    update mongoqueue

--- db.js
+++ db.js
@@ -9,13 +9,13 @@ let weed_host = process.env.WEED_HOST || 'localhost';
 
 var uri = 'mongodb://'+mongo_host+'/jude-dev';
 if(!global.db) {
-    global.db = mongoose.createConnection(uri);
-    global.db.mods = {};
-    global.judeQueue = new MongoQueue2(db, "jude-queue2");
-    global.weedClient     = new weed({
-        server:     weed_host,
-        port:       9333
-    });
+  global.db = mongoose.createConnection(uri);
+  global.db.mods = {};
+  global.judeQueue = new MongoQueue2(db, "jude-queue2");
+  global.weedClient     = new weed({
+      server:     weed_host,
+      port:       9333
+  });
 }
 
 module.exports = global.db;
