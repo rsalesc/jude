020ec57cf87c4773baf2a6c9a7bd8699fd852b2a
Roberto <iseme.beto@gmail.com>
2018-10-10 10:42:21 -0300

    Make publish script even simpler

--- publish.sh
+++ publish.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-git checkout -b latest
-
 git merge master -m "Merge before generating files ($1)" -X theirs
 git add -f build/ public/js/bulma/ public/js/admin/bundle.js
 
-echo "Commiting generated files..."
 git commit -m "Publish generated files ($1)" --no-verify
