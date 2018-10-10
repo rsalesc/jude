8c0ce01120c2ca518731b111cb263adea7ae58e5
Roberto <iseme.beto@gmail.com>
2018-10-10 10:51:49 -0300

    Check if publish.sh is run from the latest branch

--- publish.sh
+++ publish.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git merge master -m "Merge before generating files ($1)" -X theirs
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$CURRENT_BRANCH" != *latest* ]]; then
+  echo "Not in latest branch!"
+  exit 1
+fi
+
+git merge master -m "PUBLISH/MERGE: generated files ($1)" -X theirs
 git add -f build/ public/js/bulma/ public/js/admin/bundle.js
 
-git commit -m "Publish generated files ($1)" --no-verify
+git commit -m "PUBLISH: generated files ($1)" --no-verify
